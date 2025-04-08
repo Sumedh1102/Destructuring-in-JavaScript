81110be4a9ac48bc91e482ce9e9959bbcd1553c6
SUMEDH1234567 <124816595+Sumedh1102@users.noreply.github.com>
2025-04-08 23:47:40 +0530

    Main

---
+++ Destructuring/Example.js
@@ -0,0 +1,20 @@
+// Swap the values of Two variables 
+
+a = 1;
+b = 2;
+
+[a,b] = [b,a];
+
+console.log(a)
+console.log(b)
+
+
+// Swap the values of Two variables in Array
+
+const person = ["sumedh","Aryan","Mandar","Viraj"];
+
+[person[0],person[2]] = [person[2],person[0]]
+
+console.log(person)
+
+
